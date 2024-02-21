d5cce553805c6c29c0b725d915826064e88db613
tidwall <joshbaker77@gmail.com>
2024-02-21 07:57:14 -0700

    Support windows

--- test.c
+++ test.c
@@ -6,17 +6,20 @@
 int main(void) {
     // Get 1000 stacks. Write data, read data, and put them back.
     const int N = 1000;
+    const size_t STACKSZ = 262144;
     struct stack_mgr mgr;
-    stack_mgr_init(&mgr, 0);
+    struct stack_opts opts = { .stacksz = STACKSZ };
+    stack_mgr_init(&mgr, &opts);
     struct stack stacks[N];
     for (int i = 0 ; i < N; i++) {
+        
         assert(stack_get(&mgr, &stacks[i]) == 0);
     }
     for (int i = 0 ; i < N; i++) {
         char *addr = stack_addr(&stacks[i]);
         size_t size = stack_size(&stacks[i]);
         assert(addr);
-        assert(size == 8388608);
+        assert(size == STACKSZ);
         memset(addr, 99, size);
         for (size_t j = 0; j < 5000; j++) {
             assert(addr[j] == 99);
